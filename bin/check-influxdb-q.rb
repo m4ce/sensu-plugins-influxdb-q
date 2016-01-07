3fa720a34888a39888e84c4058dffd017a6a1843
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2016-01-07 23:35:20 +0100

    should be client, not source!

--- bin/check-influxdb-q.rb
+++ bin/check-influxdb-q.rb
@@ -136,23 +136,23 @@ class CheckInfluxDbQ < Sensu::Plugin::Check::CLI
     end
   end
 
-  def send_ok(check_name, source, msg)
-    event = {"name" => check_name, "source" => source, "status" => 0, "output" => "OK: #{msg}", "handler" => config[:handler]}
+  def send_ok(check_name, client, msg)
+    event = {"name" => check_name, "client" => client, "status" => 0, "output" => "OK: #{msg}", "handler" => config[:handler]}
     send_client_socket(event.to_json)
   end
 
-  def send_warning(check_name, source, msg)
-    event = {"name" => check_name, "source" => source, "status" => 1, "output" => "WARNING: #{msg}", "handler" => config[:handler]}
+  def send_warning(check_name, client, msg)
+    event = {"name" => check_name, "client" => client, "status" => 1, "output" => "WARNING: #{msg}", "handler" => config[:handler]}
     send_client_socket(event.to_json)
   end
 
-  def send_critical(check_name, source, msg)
-    event = {"name" => check_name, "source" => source, "status" => 2, "output" => "CRITICAL: #{msg}", "handler" => config[:handler]}
+  def send_critical(check_name, client, msg)
+    event = {"name" => check_name, "client" => client, "status" => 2, "output" => "CRITICAL: #{msg}", "handler" => config[:handler]}
     send_client_socket(event.to_json)
   end
 
   def send_unknown(check_name, source, msg)
-    event = {"name" => check_name, "source" => source, "status" => 3, "output" => "UNKNOWN: #{msg}", "handler" => config[:handler]}
+    event = {"name" => check_name, "client" => client, "status" => 3, "output" => "UNKNOWN: #{msg}", "handler" => config[:handler]}
     send_client_socket(event.to_json)
   end
 
